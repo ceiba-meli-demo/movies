09a5688641ea8b03a27dbe6c44667d4e69cf52ad
maik101010 <maiksoft10@gmail.com>
2020-03-31 12:41:30 -0500

    Se crea mapper para mapeo de datos entre model y modelo de datos, se crean las funciones para crear, obtener y obtener por id

---
+++ infrastructure/mappers/movie_mapper/movie_mapper.go
@@ -0,0 +1,31 @@
+package movie_mapper
+
+import (
+	"github.com/ceiba-meli-demo/movies/domain/model"
+	"github.com/ceiba-meli-demo/movies/infrastructure/adapters/repository/models"
+)
+
+func MovieToMovieDb(movie model.Movie) models.MovieDb {
+	var movieDb models.MovieDb
+	movieDb.Title = movie.Title
+	movieDb.Duration = movie.Duration
+	movieDb.Synopsis = movie.Synopsis
+	return movieDb
+}
+
+	func MovieDbToMovie(movieDb models.MovieDb) model.Movie {
+	var movie model.Movie
+	movie.Title = movieDb.Title
+	movie.Duration = movieDb.Duration
+	movie.Synopsis = movieDb.Synopsis
+	return movie
+}
+
+func MoviesDbToMovies(moviesDb []models.MovieDb) []model.Movie {
+	var movies []model.Movie
+	for _, movieDb := range moviesDb {
+		movie := MovieDbToMovie(movieDb)
+		movies = append(movies, movie)
+	}
+	return movies
+}
