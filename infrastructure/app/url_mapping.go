fe7bc0c9688bbd7412834983a52cea3443402e64
Francisco Marin <francisco.marin@ceiba.com.co>
2020-04-02 19:24:37 -0500

    se completa el consumo de los servicios de users
    se integra users a movie

--- infrastructure/app/url_mapping.go
+++ infrastructure/app/url_mapping.go
@@ -1,9 +1,15 @@
 package app
 
-import "github.com/ceiba-meli-demo/movies/infrastructure/controllers"
+import (
+	"github.com/ceiba-meli-demo/movies/infrastructure/app/middlewares/users"
+	"github.com/ceiba-meli-demo/movies/infrastructure/controllers"
+)
 
 func mapUrls(handler controllers.RedirectMovieHandler) {
 	router.GET("/movies", handler.Get)
 	router.GET("/movies/:movie_id", handler.FindByID)
-	router.POST("/movies", handler.Create)
+	router.Use(users.UserRequired())
+	{
+		router.POST("/movies", handler.Create)
+	}
 }
--- infrastructure/controllers/movie_controller.go
+++ infrastructure/controllers/movie_controller.go
@@ -2,10 +2,12 @@ package controllers
 
 import (
 	"encoding/hex"
+	"fmt"
 	"net/http"
 
 	"github.com/ceiba-meli-demo/movies/application/commands"
 	"github.com/ceiba-meli-demo/movies/application/usescases"
+	"github.com/ceiba-meli-demo/movies/infrastructure/app/middlewares/users"
 	"github.com/ceiba-meli-demo/movies/infrastructure/utils/rest_errors"
 	"github.com/gin-gonic/gin"
 )
@@ -53,6 +55,7 @@ func (handler *Handler) FindByID(c *gin.Context) {
 //Create method controller, get json type movie
 func (handler *Handler) Create(c *gin.Context) {
 	var movieCommand commands.MovieCommand
+	fmt.Println(movieCommand)
 	if err := c.ShouldBindJSON(&movieCommand); err != nil {
 		restErr := rest_errors.NewBadRequestError("invalid json")
 		c.JSON(restErr.Status(), restErr)
@@ -61,6 +64,10 @@ func (handler *Handler) Create(c *gin.Context) {
 	result, createMovieErr := handler.CreateMovieUseCase.Handler(movieCommand)
 	if createMovieErr != nil {
 		_ = c.Error(createMovieErr)
+		return
+	}
+	if _, err := users.UpdateUser(movieCommand.Dni); err != nil {
+		_ = c.Error(err)
 		return
 	}
 	c.JSON(http.StatusCreated, result)
