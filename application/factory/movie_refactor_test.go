dd05aaeb3820766aceec6a10495988dba6b43232
Francisco Marin <francisco.marin@ceiba.com.co>
2020-04-03 15:11:57 -0500

    test movie refactor
    se agrega test a movie refactor en la carpeta application

---
+++ application/factory/movie_refactor_test.go
@@ -0,0 +1,31 @@
+package factory
+
+import (
+	"testing"
+
+	"github.com/ceiba-meli-demo/movies/application/commands"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGoodMovieFactory(t *testing.T) {
+	var cmd commands.MovieCommand
+	cmd.Movie.Title = "Eramos muchos y se preño la abuela"
+	cmd.Movie.Duration = 120
+	cmd.Movie.Synopsis = "Triste historia de la vida real"
+
+	movie, err := CreateMovie(cmd)
+	assert.NotNil(t, cmd)
+	assert.NotNil(t, movie)
+	assert.True(t, err == nil, "err is equal to nil")
+}
+
+func TestBadMovieFactory(t *testing.T) {
+	var cmd commands.MovieCommand
+	cmd.Movie.Title = "Eramos muchos y se preño la abuela 2"
+	cmd.Movie.Duration = 140
+
+	movie, err := CreateMovie(cmd)
+	assert.NotNil(t, cmd)
+	assert.NotNil(t, movie)
+	assert.True(t, err != nil, "err is present because synopsis is blank")
+}
